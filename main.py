f611fc899e192d4d998fd00bf7f24178fbbe6fd8
anikam21 <anikamarzia2121@hotmail.com>
2025-02-11 00:22:22 -0500

    adding additional movements

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ x = 100
 # Importing an image 
 player_surf = pygame.image.load(PLAYER_IMAGE).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(2, -1)
+player_direction = pygame.math.Vector2(1, 1)
 
 
 star_surf = pygame.image.load(STAR_IMAGE).convert_alpha()
@@ -33,7 +33,6 @@ laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 while running:
     # frame rate (s)
     dt = clock.tick() /1000
-    print(dt)
     # event loop
     for event in pygame.event.get():
         # user clicked X to close the window
@@ -50,6 +49,10 @@ while running:
 
     # if the player is outside of the frame then we change directions
     # player movement
+    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
+        player_direction.y *= -1
+    if player_rect.left <= 0 or player_rect.right >= WINDOW_WIDTH:
+        player_direction.x *= -1
     player_rect.center += player_direction * PLAYER_SPEED *dt
     display_surface.blit(player_surf, player_rect)
 
