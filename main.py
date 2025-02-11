d3f462f1a1f741dcfe2f79503ecb36d49265e143
anikam21 <anikamarzia2121@hotmail.com>
2025-02-10 20:52:41 -0500

    adding main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption(TITLE)
 # when the game is running 
 running = True
+clock = pygame.time.Clock()
 
 # Plain Surface
 surf = pygame.Surface((100,200))
@@ -17,6 +18,8 @@ x = 100
 # Importing an image 
 player_surf = pygame.image.load(PLAYER_IMAGE).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(2, -1)
+
 
 star_surf = pygame.image.load(STAR_IMAGE).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
@@ -24,8 +27,13 @@ star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in
 meteor_surf = pygame.image.load(METEOR_IMAGE).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = ((WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)))
 
+laser_surf = pygame.image.load(LASER_IMAGE).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
+    # frame rate (s)
+    dt = clock.tick() /1000
+    print(dt)
     # event loop
     for event in pygame.event.get():
         # user clicked X to close the window
@@ -37,14 +45,16 @@ while running:
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
 
-    if player_rect.right < WINDOW_WIDTH:
-            player_rect.left += 0.2
+    display_surface.blit(meteor_surf, meteor_rect)
+
+
+    # if the player is outside of the frame then we change directions
+    # player movement
+    player_rect.center += player_direction * PLAYER_SPEED *dt
     display_surface.blit(player_surf, player_rect)
 
+    display_surface.blit(laser_surf, laser_rect)
     
-    display_surface.blit(meteor_surf, meteor_rect)
-    
-
     # updates the full window
     pygame.display.update()
 
--- settings.py
+++ settings.py
@@ -5,9 +5,13 @@ WINDOW_HEIGHT = 720
 TIME = 60
 TITLE = 'Rocketship Shooting Game'
 
+#PLAYER SETTINGS
+PLAYER_SPEED = 300 
+
 BASE_DIR = os.path.dirname(__file__)
 
 # Image Paths
 PLAYER_IMAGE = os.path.join(BASE_DIR, 'images', 'player.png')
 STAR_IMAGE = os.path.join(BASE_DIR, 'images', 'star.png')
 METEOR_IMAGE = os.path.join(BASE_DIR, 'images', 'meteor.png')
+LASER_IMAGE = os.path.join(BASE_DIR, 'images', 'laser.png')
